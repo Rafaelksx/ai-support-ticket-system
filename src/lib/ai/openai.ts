bdcd4d393e9f0bd7954821ad73d425d7eaa44896
Rafaelksx <rr490233@gmail.com>
2026-05-27 20:16:06 -0400

    Fix dummy api key

--- src/lib/ai/openai.ts
+++ src/lib/ai/openai.ts
@@ -4,5 +4,5 @@ import OpenAI from 'openai';
 // Note: This must only be run server-side (API Routes, Server Actions, Server Components)
 // because it relies on the private process.env.OPENAI_API_KEY.
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: process.env.OPENAI_API_KEY || 'dummy_key_to_allow_ui_render_without_crashing',
 });
